38c05a025b573ded2d9f399198d10f2c60286b7d
Will <wneal_19@yahoo.co.uk>
2023-02-20 04:41:32 +0000

    Config for Stimulus and Reflex

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,16 +2,14 @@ import { Application } from "@hotwired/stimulus"
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import StimulusReflex from 'stimulus_reflex'
 import consumer from '../channels/consumer'
-
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
+application.debug = true
 window.Stimulus   = application
 
-const context = require.context('controllers', true, /_controller\.js$/)
+const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
 application.consumer = consumer
-StimulusReflex.initialize(application, { isolate: true })
-
+StimulusReflex.initialize(application, { isolate: true, debug: true })
 export { application }
