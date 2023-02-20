bfb71fe9128d8a19d5d63b0741137e5b8d830af4
Will <wneal_19@yahoo.co.uk>
2023-02-20 04:43:45 +0000

    Removed duplicate session store

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  config.action_controller.default_url_options = {host: "localhost", port: 3000}
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -63,8 +66,6 @@ Rails.application.configure do
     driver: :hiredis,
     url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }
   }
-  
-  config.session_store :cache_store, key: "_sessions_development", compress: true, pool_size: 5, expire_after: 1.year
   
   config.session_store(
     :cache_store,
